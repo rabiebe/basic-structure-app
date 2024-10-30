f8293fdfb321fa64c25e5a4ff1c37527efd939f7
rabiebe <rabiebelkaida@gmail.com>
2024-10-30 15:03:30 +0100

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
+
+export default function HomeScreen() {
+  return (
+    <Center>
+      <Text className="text-2xl font-bold text-primary-600">
+        Soccer booking app
+      </Text>
+    </Center>
+  );
+}
